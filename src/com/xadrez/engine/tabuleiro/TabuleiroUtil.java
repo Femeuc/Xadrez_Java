ed93a4bffb3791d877e39614f3f0adf6a0425e0c
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-01-21 14:02:04 -0300

    Criei o tabuleiro e os seus 64 quadrados na GUI

--- src/com/xadrez/engine/tabuleiro/TabuleiroUtil.java
+++ src/com/xadrez/engine/tabuleiro/TabuleiroUtil.java
@@ -7,8 +7,14 @@ public class TabuleiroUtil {
     public static final boolean[] SETIMA_COLUNA = iniciarColuna(6);
     public static final boolean[] OITAVA_COLUNA = iniciarColuna(7);
 
+    public static final boolean[] PRIMEIRA_LINHA = iniciarLinha(0);
     public static final boolean[] SEGUNDA_LINHA = iniciarLinha(8);   // ID do quadrado em que a linha começa
+    public static final boolean[] TERCEIRA_LINHA = iniciarLinha(16);
+    public static final boolean[] QUARTA_LINHA = iniciarLinha(24);
+    public static final boolean[] QUINTA_LINHA = iniciarLinha(32);
+    public static final boolean[] SEXTA_LINHA = iniciarLinha(40);
     public static final boolean[] SETIMA_LINHA = iniciarLinha(48);
+    public static final boolean[] OITAVA_LINHA = iniciarLinha(56);
 
     public static final int QUANTIDADE_QUADRADOS = 64;
     public static final int QUADRADOS_POR_LINHA = 8;
--- src/com/xadrez/gui/Tabela.java
+++ src/com/xadrez/gui/Tabela.java
@@ -1,27 +1,40 @@
 package com.xadrez.gui;
 
+import com.xadrez.engine.tabuleiro.TabuleiroUtil;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tabela {
 
     private final JFrame gameFrame;
+    private final PainelDoTabuleiro painelDoTabuleiro;
+    private Color quadradoCorClara = Color.decode("#FFFACD");
+    private Color quadradoCorEscura = Color.decode("#593E1A");
 
-    private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+    private final static Dimension DIMENSAO_DO_QUADRO_EXTERIOR = new Dimension(600, 600);
+    private final static Dimension DIMENSAO_DO_PAINEL_DO_TABULEIRO = new Dimension(400, 350);
+    private final static Dimension DIMENSAO_DO_PAINEL_DE_QUADRADO = new Dimension(10, 10);
 
     public Tabela() {
         this.gameFrame = new JFrame("JXadrez");
-        final JMenuBar barraDeMenuDaTabela = new JMenuBar();
-        popularBarraDeMenu(barraDeMenuDaTabela);
+        this.gameFrame.setLayout(new BorderLayout());
+        final JMenuBar barraDeMenuDaTabela = criarBarraDeMenuDaTabela();
         this.gameFrame.setJMenuBar(barraDeMenuDaTabela);
-        this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+        this.gameFrame.setSize(DIMENSAO_DO_QUADRO_EXTERIOR);
+        this.painelDoTabuleiro = new PainelDoTabuleiro();
+        this.gameFrame.add(this.painelDoTabuleiro, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
 
-    private void popularBarraDeMenu(final JMenuBar barraDeMenuDaTabela) {
+    private JMenuBar criarBarraDeMenuDaTabela() {
+        final JMenuBar barraDeMenuDaTabela = new JMenuBar();
         barraDeMenuDaTabela.add(criarMenuDeArquivo());
+        return barraDeMenuDaTabela;
     }
 
     private JMenu criarMenuDeArquivo() {
@@ -34,6 +47,59 @@ public class Tabela {
             }
         });
         menuDeArquivo.add(openPGN);
+
+        final JMenuItem menuFechar = new JMenuItem("Fechar");
+        menuFechar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+        menuDeArquivo.add(menuFechar);
+
         return menuDeArquivo;
+    }
+
+    private class PainelDoTabuleiro extends JPanel {
+        final List<PainelDeQuadrado> quadradosDoTabuleiro;
+
+        PainelDoTabuleiro() {
+            super(new GridLayout(8,8));
+            this.quadradosDoTabuleiro = new ArrayList<>();
+            for(int i = 0; i < TabuleiroUtil.QUANTIDADE_QUADRADOS; i++) {
+                final PainelDeQuadrado painelDeQuadrado = new PainelDeQuadrado(this, i);
+                this.quadradosDoTabuleiro.add(painelDeQuadrado);
+                add(painelDeQuadrado);
+            }
+            setPreferredSize(DIMENSAO_DO_PAINEL_DO_TABULEIRO);
+            validate();
+        }
+    }
+
+    private class PainelDeQuadrado extends JPanel {
+        private final int quadradoID;
+
+        PainelDeQuadrado(final PainelDoTabuleiro painelDoTabuleiro,
+                         final int quadradoID) {
+            super(new GridBagLayout());
+            this.quadradoID = quadradoID;
+            setPreferredSize(DIMENSAO_DO_PAINEL_DE_QUADRADO);
+            atribuirCorDoQuadrado();
+            validate();
+        }
+
+        private void atribuirCorDoQuadrado() {
+            if(TabuleiroUtil.PRIMEIRA_LINHA[this.quadradoID] ||
+                    TabuleiroUtil.TERCEIRA_LINHA[this.quadradoID] ||
+                    TabuleiroUtil.QUINTA_LINHA[this.quadradoID] ||
+                    TabuleiroUtil.SETIMA_LINHA[this.quadradoID]) {
+                setBackground(this.quadradoID % 2 == 0 ? quadradoCorClara : quadradoCorEscura);
+            } else if(TabuleiroUtil.SEGUNDA_LINHA[this.quadradoID] ||
+                        TabuleiroUtil.QUARTA_LINHA[this.quadradoID] ||
+                        TabuleiroUtil.SEXTA_LINHA[this.quadradoID] ||
+                        TabuleiroUtil.OITAVA_LINHA[this.quadradoID]) {
+                setBackground(this.quadradoID % 2 != 0 ? quadradoCorClara : quadradoCorEscura);
+            }
+        }
     }
 }
